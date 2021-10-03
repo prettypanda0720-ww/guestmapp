d0c6e145de09867d3ddf0ab05ff240d6827f87c5
prettypanda0720-ww <59716106+prettypanda0720-ww@users.noreply.github.com>
2021-10-03 21:20:27 +0300

    Update settings.py

--- guestmapp/settings.py
+++ guestmapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8bhn+&2j)iqkec*0nepyi!1s3+4tn^towp&rc7p4rmfw1)hq+o'
+SECRET_KEY = 'as8bhn+&2j)iqkec*0nepyi!1s3+4tn^towp&rc7p4rmfw1)hq+odg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
